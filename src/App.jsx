6464c23435b288a89a30894fe5abe1d3159d1d11
ZeinabHalipa <jobbie008@gmail.com>
2025-05-06 15:21:54 +0800

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Blog from './components/Blog';
 import Projects from './components/Projects';
 import C from './components/C';
 import Experience from './components/Experience';
+import Certification from './components/Certification';
 import './App.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Hero />} />
         <Route path="/about" element={<AboutMe />} />
         <Route path="/blog" element={<Blog />} />
+        <Route path="/cert" element={<Certification />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/experience" element={<Experience />} />
         <Route path="/c" element={<C />} />
--- src/components/projectsData.jsx
+++ src/components/projectsData.jsx
@@ -1,18 +1,29 @@
 import pic1 from "../assets/img/pkd.png";
-
+import pic2 from "../assets/img/snhs.png";
 
 const projectsData = [
     {
       id: 1,
       title: "Pokedex",
       description: "A full-featured online store with cart functionality, user authentication, and payment processing.",
-      technologies: ["React", "Node.js", "MongoDB", "Stripe API"],
+      technologies: ["React", "Node.js", "Stripe API"],
       imageUrl: pic1,
       githubLink: "#",
       liveDemo: "#",
       category: "frontend",
     },
     
+    {
+      id: 2,
+      title: "Southcom National High School Information Management System",
+      description: "A comprehensive information management system developed for Southcom National High School.",
+      technologies: ["HTML", "CSS", "Javascript", "MySQL"],
+      imageUrl: pic2,
+      githubLink: "#",
+      liveDemo: "#",
+      category: "frontend",
+    },
+
   ];
   
   export default projectsData;
